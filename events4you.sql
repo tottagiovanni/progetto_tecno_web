5dce1936e23f35670b61d48ab40aa3acb68f82c5
AlbertoSaraceno <40261786+AlbertoSaraceno@users.noreply.github.com>
2021-05-03 18:50:08 +0200

    Aggiunta modifiche Giovanni

--- events4you.sql
+++ events4you.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 28, 2021 alle 17:05
+-- Creato il: Mag 03, 2021 alle 10:41
 -- Versione del server: 10.4.18-MariaDB
 -- Versione PHP: 8.0.3
 
@@ -24,56 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `evento`
+-- Struttura della tabella `analisi`
 --
 
-CREATE TABLE `evento` (
-  `id_evento` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_evento`),
-  `titolo` varchar(50) COLLATE utf8_bin NOT NULL,
-  `descrizione` text COLLATE utf8_bin NOT NULL,
-  `luogo` varchar(32) COLLATE utf8_bin NOT NULL,
-  `data` date NOT NULL,
-  `disponibilita` smallint(5) UNSIGNED NOT NULL,
-  `partecipazione` smallint(5) UNSIGNED NOT NULL,
-  `prezzo` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dump dei dati per la tabella `evento`
---
-
-INSERT INTO `evento` (`id_evento`, `titolo`, `descrizione`, `luogo`, `data`, `disponibilita`, `partecipazione`, `prezzo`) VALUES
-(1, 'Giovanni Live', 'Ciao', 'San Giovanni Rotondo', '2021-04-02', 1000, 23, '25.00');
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `faq`
---
-
-CREATE TABLE `faq` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  `domanda` text COLLATE utf8_bin NOT NULL,
-  `risposta` text COLLATE utf8_bin NOT NULL
+CREATE TABLE `analisi` (
+  `id_utente` varchar(32) COLLATE utf8_bin NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `biglietti_venduti` int(10) UNSIGNED NOT NULL,
+  `ricavo` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
-
-
-
---
--- Struttura della tabella `utente`
---
-
-CREATE TABLE `utente` (
-  `username` varchar(32) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`username`),
-  `password` varchar(32) COLLATE utf8_bin NOT NULL,
-  `nome` varchar(50) COLLATE utf8_bin NOT NULL,
-  `livello` tinyint(3) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Struttura della tabella `biglietto`
@@ -82,40 +43,43 @@ CREATE TABLE `utente` (
 CREATE TABLE `biglietto` (
   `id_utente` varchar(32) COLLATE utf8_bin NOT NULL,
   `id_evento` int(11) NOT NULL,
-  PRIMARY KEY (`id_utente`,`id_evento`),
   `numero` smallint(5) UNSIGNED NOT NULL,
   `data_acquisto` date NOT NULL,
   `mod_pagamento` varchar(20) COLLATE utf8_bin NOT NULL,
-  `prezzo_totale` decimal(10,2) NOT NULL,
-  FOREIGN KEY (`id_utente`)
-  	REFERENCES `utente` (`username`)
-  	ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`id_evento`)
-  	REFERENCES `evento` (`id_evento`)
-  	ON DELETE CASCADE ON UPDATE CASCADE
+  `prezzo_totale` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 
 -- --------------------------------------------------------
 
-
 --
--- Struttura della tabella `analisi`
+-- Struttura della tabella `evento`
 --
 
-CREATE TABLE `analisi` (
-  `id_utente` varchar(32) NOT NULL,
+CREATE TABLE `evento` (
   `id_evento` int(11) NOT NULL,
-  PRIMARY KEY (`id_utente`,`id_evento`),
-  `biglietti_venduti` int(10) UNSIGNED NOT NULL,
-  `ricavo` decimal(10,2) NOT NULL,
-  FOREIGN KEY (`id_utente`)
-  	REFERENCES `utente` (`username`)
-  	ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`id_evento`)
-  	REFERENCES `evento` (`id_evento`)
-  	ON DELETE CASCADE ON UPDATE CASCADE
+  `titolo` varchar(50) COLLATE utf8_bin NOT NULL,
+  `descrizione` text COLLATE utf8_bin NOT NULL,
+  `luogo` varchar(32) COLLATE utf8_bin NOT NULL,
+  `data` date NOT NULL,
+  `disponibilita` smallint(5) UNSIGNED NOT NULL,
+  `partecipazione` smallint(5) UNSIGNED NOT NULL,
+  `prezzo` decimal(10,2) NOT NULL,
+  `mappa` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `faq`
+--
+
+CREATE TABLE `faq` (
+  `id` int(11) NOT NULL,
+  `domanda` text COLLATE utf8_bin NOT NULL,
+  `risposta` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
 
 --
 -- Struttura della tabella `last_minute`
@@ -123,13 +87,133 @@ CREATE TABLE `analisi` (
 
 CREATE TABLE `last_minute` (
   `id_evento` int(11) NOT NULL,
-  PRIMARY KEY (`id_evento`),
   `percentuale` tinyint(3) UNSIGNED NOT NULL,
-  `data_inizio` date NOT NULL,
-  FOREIGN KEY (`id_evento`)
-  	REFERENCES `evento` (`id_evento`)
-  	ON DELETE CASCADE ON UPDATE CASCADE
+  `data_inizio` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `username` varchar(32) COLLATE utf8_bin NOT NULL,
+  `password` varchar(32) COLLATE utf8_bin NOT NULL,
+  `nome` varchar(50) COLLATE utf8_bin NOT NULL,
+  `livello` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `partecipazione`
+--
+
+CREATE TABLE `partecipazione` (
+  `id_utente` varchar(32) COLLATE utf8_bin NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `partecipante` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `analisi`
+--
+ALTER TABLE `analisi`
+  ADD PRIMARY KEY (`id_utente`,`id_evento`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indici per le tabelle `biglietto`
+--
+ALTER TABLE `biglietto`
+  ADD PRIMARY KEY (`id_utente`,`id_evento`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indici per le tabelle `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id_evento`);
+
+--
+-- Indici per le tabelle `faq`
+--
+ALTER TABLE `faq`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `last_minute`
+--
+ALTER TABLE `last_minute`
+  ADD PRIMARY KEY (`id_evento`);
+
+--
+-- Indici per le tabelle `partecipazione`
+--
+ALTER TABLE `partecipazione`
+  ADD PRIMARY KEY (`id_utente`,`id_evento`);
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `faq`
+--
+ALTER TABLE `faq`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `analisi`
+--
+ALTER TABLE `analisi`
+  ADD CONSTRAINT `analisi_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `analisi_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `biglietto`
+--
+ALTER TABLE `biglietto`
+  ADD CONSTRAINT `biglietto_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `biglietto_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+--
+-- Limiti per la tabella `partecipazione`
+--
+ALTER TABLE `partecipazione`
+  ADD CONSTRAINT `partecipazione_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `partecipazione_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `last_minute`
+--
+ALTER TABLE `last_minute`
+  ADD CONSTRAINT `last_minute_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
